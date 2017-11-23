50e621d36d5688f0ad3544c3d13b7957c5e02dd8
monzo94 <monzo_94@hotmail.com>
2017-11-23 19:45:16 +0100

    Add problem 14

---
+++ problem14.hs
@@ -0,0 +1,18 @@
+-- Problem 14: Find the longest Collatz chain with starting number under one million
+import Data.List
+import Data.Ord
+
+-- Computes Collatz sequence given a starting natural number
+collatz :: (Integral a) => a -> [a]
+collatz 1 = 1:[]
+collatz n
+  | even n = n:collatz (n `div` 2)
+  | odd n = n:collatz (3*n + 1)
+
+-- Gets longest Collatz chain with starting value strictly smaller than n
+longestCollatz :: (Integral a) => a -> [a]
+longestCollatz n = maximumBy (comparing length) $ collatzSeq n
+    where collatzSeq n = map collatz [1..n-1]
+
+main :: IO ()
+main = do print . head . longestCollatz $ 1000000
